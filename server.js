b4793529e00403551cb4c656c7e7994ac1bc3980
VAISHNAVREDDYDANDY <ldandu1@charlotte.edu>
2025-10-21 12:05:02 -0400

    initial

---
+++ server.js
@@ -0,0 +1,21 @@
+//Budget API
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use('/', express.static('public'));
+
+app.use(cors());
+
+const budget = require('./budget-data.json');
+
+
+app.get('/budget', (req, res) => {
+    res.json(budget);
+});
+
+app.listen(port, () => {
+    console.log(`API listening at http://localhost:${port}`);
+});
